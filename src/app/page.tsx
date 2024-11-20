08267f66962787213e9f7485acd01e963236683d
yeyiwon <4576874@naver.com>
2024-11-20 17:58:43 +0900

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      
+      <SpeedInsights/>
+      
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -96,6 +100,8 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+    
+    
     </div>
   );
 }
